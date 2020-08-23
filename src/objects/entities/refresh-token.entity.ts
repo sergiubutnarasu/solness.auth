a91875069c937f3d10a57bd56663832a31d1ff9e
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2020-08-23 20:43:26 +0300

    Add user relation

--- src/objects/entities/refresh-token.entity.ts
+++ src/objects/entities/refresh-token.entity.ts
@@ -1,10 +1,13 @@
 import { BaseEntity } from '@solness/core';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity('refreshToken')
 export class RefreshToken extends BaseEntity {
   @Column()
   userId: number;
+
+  @ManyToOne('User', { nullable: false })
+  user?: any;
 
   @Column({ length: 250 })
   token: string;
